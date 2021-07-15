d9127f88db8c4285603d12becb15cdab44cfa7d8
qlanti <1524dd.xio@gmail.com>
2021-07-15 16:34:49 +0900

    エリアページにページネーションを追加

--- pages/area/_slug.vue
+++ pages/area/_slug.vue
@@ -2,7 +2,7 @@
   <section>
     <v-row dense>
       <card
-        v-for="(pub, i) in pubs"
+        v-for="(pub, i) in displayPubs"
         :key="i"
         :name="pub.fields.name"
         :id="pub.sys.id"
@@ -10,6 +10,13 @@
         :link="pub.fields.link"
         :image="pub.fields.image"
       />
+      <v-pagination
+        class="mx-auto"
+        v-model="page"
+        :length="length"
+        circle
+        @input="pageChange"
+      ></v-pagination>
     </v-row>
   </section>
 </template>
@@ -19,20 +26,35 @@ import CONSTANTS from '~/lib/constants.js'
 import { createClient } from '~/plugins/contentful.js'
 
 export default {
-  async asyncData({ params }) {
-    return await createClient()
+  async asyncData({ params, route }) {
+    const pubs = (await createClient()
       .getEntries({
         content_type: CONSTANTS.CTF_PUB_TYPE_ID,
         "fields.area.sys.contentType.sys.id": "area",
         "fields.area.fields.slug": params.slug
       })
-      .then(entries => {
-        return {
-          pubs: entries.items
-        }
-      })
-      .catch(console.error)
-  }
+    ).items
+
+    const length = Math.ceil(pubs.length / CONSTANTS.AREAS_PER_PAGE)
+
+    const displayPubs = pubs.slice(0, CONSTANTS.AREAS_PER_PAGE)
+
+    return {
+      page: 1,
+      pubs: pubs,
+      length: length,
+      displayPubs: displayPubs,
+    }
+  },
+
+  methods: {
+    pageChange: function(pageNumber) {
+      this.displayPubs = this.pubs.slice(
+        CONSTANTS.AREAS_PER_PAGE * (pageNumber - 1),
+        CONSTANTS.AREAS_PER_PAGE * pageNumber
+      )
+    }
+  },
 }
 </script>
 
